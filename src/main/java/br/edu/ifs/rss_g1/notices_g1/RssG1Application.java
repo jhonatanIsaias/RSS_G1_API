5c861f0c51636a87c1af07a319942e34dca580fd
Jhonatan Isaias Dos Santos <jhonatan.santos13@academico.ifs.edu.br>
2024-05-06 21:57:45 -0300

    project created

---
+++ src/main/java/br/edu/ifs/rss_g1/notices_g1/RssG1Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.rss_g1.notices_g1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssG1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RssG1Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifs/rss_g1/notices_g1/RssG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.rss_g1.notices_g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssG1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
